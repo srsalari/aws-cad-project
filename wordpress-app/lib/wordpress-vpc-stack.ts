9f9d8a52065f660f2b0bf6567e546e9027a06f9c
srsalari <sr_salari@hotmail.com>
2025-01-29 20:51:09 -0500

    Update wordpress-vpc-stack.ts
    Add completed parameters exchange in VPC stack

--- wordpress-app/lib/wordpress-vpc-stack.ts
+++ wordpress-app/lib/wordpress-vpc-stack.ts
@@ -1,150 +1,145 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as rds from 'aws-cdk-lib/aws-rds';
-
-
+// import * as ssm from 'aws-cdk-lib/aws-ssm'; // Uncomment if storing outputs in SSM
 
 export class WordpressVpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'WordpressAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
-    //VPC with SSM Param
-
+    /**
+     * 1) Create a new Virtual Private Cloud (VPC)
+     *    - CIDR: 10.50.0.0/16
+     *    - Includes Public and Private subnets across multiple AZs
+     *    - Internet Gateway is created for public subnets
+     */
     const customVpc = new ec2.Vpc(this, 'CustomVPC', {
       ipAddresses: ec2.IpAddresses.cidr('10.50.0.0/16'),
       createInternetGateway: true,
     });
 
-    
-    // const ssmVPC = new ssm.StringParameter(this, 'vpcSsmParameter', {
-    //   parameterName: '/AWS/CAD/VPC/ID',
-    //   stringValue: customVpc.vpcId,
-    // });
-
-  //   const cfnParameter = new ssm.CfnParameter(this, 'MyCfnParameter', {
-  //     type: 'String',
-  //     name: '/AWS/CAD/VPC/ID',
-  //     value: 'customVpc.vpcId',
-  //  });
-
-   new cdk.CfnOutput(this, 'CustomVPCIDOutput', {
-    value: customVpc.vpcId,
-    exportName: 'Application-VPC-ID', // optional if you want to share this output across stacks
-  });
-
-  const publicSubnetIds = customVpc.publicSubnets.map(subnet => subnet.subnetId);
-
-  // Output the public subnet IDs
-  new cdk.CfnOutput(this, 'CustomVPCPublicSubnetIds', {
-    value: JSON.stringify(publicSubnetIds),
-    exportName: 'Application-PUBLIC-SUBNET-IDS',
-  });
-    //SG-ALB with SSM Param
-
-    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALBSecurityGroup', { 
-      vpc: customVpc
+    /**
+     * 2) Output the VPC ID so that other stacks can import it.
+     */
+    new cdk.CfnOutput(this, 'CustomVPCIDOutput', {
+      value: customVpc.vpcId,
+      exportName: 'Application-VPC-ID',
     });
 
-    albSecurityGroup.addIngressRule(
-      ec2.Peer.ipv4("0.0.0.0/0"),
-      ec2.Port.tcp(80)
-    )
+    /**
+     * 3) Retrieve public & private subnets
+     *    and export their IDs for referencing in other stacks.
+     */
+    const publicSubnets = customVpc.publicSubnets;
+    const privateSubnets = customVpc.privateSubnets;
 
+    // Public Subnet 1/2
+    new cdk.CfnOutput(this, 'PublicSubnetID1Output', {
+      value: publicSubnets[0].subnetId,
+      exportName: 'Public-Subnet1-ID',
+    });
+    new cdk.CfnOutput(this, 'PublicSubnetID2Output', {
+      value: publicSubnets[1].subnetId,
+      exportName: 'Public-Subnet2-ID',
+    });
+
+    // Private Subnet 1/2
+    new cdk.CfnOutput(this, 'PrivateSubnetID1Output', {
+      value: privateSubnets[0].subnetId,
+      exportName: 'Private-Subnet1-ID',
+    });
+    new cdk.CfnOutput(this, 'PrivateSubnetID2Output', {
+      value: privateSubnets[1].subnetId,
+      exportName: 'Private-Subnet2-ID',
+    });
+
+    /**
+     * 4) Security Group for ALB
+     *    - Allows inbound HTTP (80) & HTTPS (443) from anywhere (0.0.0.0/0).
+     */
+    const albSecurityGroup = new ec2.SecurityGroup(this, 'ALBSecurityGroup', {
+      vpc: customVpc,
+    });
     albSecurityGroup.addIngressRule(
-      ec2.Peer.ipv4("0.0.0.0/0"),
-      ec2.Port.tcp(443)
-    )
-    
-    // const ssmALBSG = new ssm.StringParameter(this, 'albSGSsmParameter', {
-    //   parameterName: '/AWS/CAD/ALB/SG/ID',
-    //   stringValue: albSecurityGroup.securityGroupId,
-    // });
-    
+      ec2.Peer.ipv4('0.0.0.0/0'),
+      ec2.Port.tcp(80),
+      'Allow HTTP from Internet'
+    );
+    albSecurityGroup.addIngressRule(
+      ec2.Peer.ipv4('0.0.0.0/0'),
+      ec2.Port.tcp(443),
+      'Allow HTTPS from Internet'
+    );
+
     new cdk.CfnOutput(this, 'ALBSGOutput', {
       value: albSecurityGroup.securityGroupId,
-      exportName: 'Application-ALB-SG-ID', // optional if you want to share this output across stacks
-    });
-    //SG-EC2 with SSM Param
-    const ec2SecurityGroup = new ec2.SecurityGroup(this, 'EC2SecurityGroup', { 
-      vpc: customVpc
+      exportName: 'Application-ALB-SG-ID',
     });
 
+    /**
+     * 5) Security Group for EC2 instances
+     *    - Allows SSH (22) from anywhere (for demo), not recommended for production.
+     *    - Allows inbound HTTP/HTTPS from the ALB SG.
+     */
+    const ec2SecurityGroup = new ec2.SecurityGroup(this, 'EC2SecurityGroup', {
+      vpc: customVpc,
+    });
     ec2SecurityGroup.addIngressRule(
-      ec2.Peer.ipv4("0.0.0.0/0"),
-      ec2.Port.tcp(22)
-    )
-
+      ec2.Peer.ipv4('0.0.0.0/0'),
+      ec2.Port.tcp(22),
+      'Allow SSH from Internet (Demo only!)'
+    );
     ec2SecurityGroup.addIngressRule(
       albSecurityGroup,
-      ec2.Port.tcp(80)
-    )
-
+      ec2.Port.tcp(80),
+      'Allow inbound HTTP from ALB'
+    );
     ec2SecurityGroup.addIngressRule(
       albSecurityGroup,
-      ec2.Port.tcp(443)
-    )
-
-    // const ssmEC2SG = new ssm.StringParameter(this, 'ec2SGSsmParameter', {
-    //   parameterName: '/AWS/CAD/EC2/SG/ID',
-    //   stringValue: ec2SecurityGroup.securityGroupId,
-    // });
+      ec2.Port.tcp(443),
+      'Allow inbound HTTPS from ALB'
+    );
 
     new cdk.CfnOutput(this, 'EC2SGOutput', {
       value: ec2SecurityGroup.securityGroupId,
-      exportName: 'Application-EC2-SG-ID', // optional if you want to share this output across stacks
-    });
-    //SG-RDS with SSM Param
-
-    const rdsSecurityGroup = new ec2.SecurityGroup(this, 'RDSSecurityGroup', { 
-      vpc: customVpc
+      exportName: 'Application-EC2-SG-ID',
     });
 
+    /**
+     * 6) Security Group for RDS MySQL
+     *    - Allows inbound traffic on port 3306 from the EC2 SG only.
+     */
+    const rdsSecurityGroup = new ec2.SecurityGroup(this, 'RDSSecurityGroup', {
+      vpc: customVpc,
+    });
     rdsSecurityGroup.addIngressRule(
       ec2SecurityGroup,
-      ec2.Port.tcp(3306)
-    )
-
-    // const ssmRDSSG = new ssm.StringParameter(this, 'RDSSGSsmParameter', {
-    //   parameterName: '/AWS/CAD/RDS/SG/ID',
-    //   stringValue: rdsSecurityGroup.securityGroupId,
-    // });
+      ec2.Port.tcp(3306),
+      'Allow MySQL from EC2 instances'
+    );
 
     new cdk.CfnOutput(this, 'RDSSGOutput', {
       value: rdsSecurityGroup.securityGroupId,
-      exportName: 'Application-RDS-SG-ID', // optional if you want to share this output across stacks
+      exportName: 'Application-RDS-SG-ID',
     });
 
-    //RDS-Subnet-Group with SSM Param
-
+    /**
+     * 7) Create an RDS Subnet Group
+     *    - This is required for placing your RDS instance in private subnets.
+     */
     const rdsSubnetGroup = new rds.SubnetGroup(this, 'RDSSubnetGroup', {
       description: 'RDS SubnetGroup',
       vpc: customVpc,
-    
-      // the properties below are optional
-      subnetGroupName: 'RDS-SUBNET-GROUP',
+      subnetGroupName: 'RDS-SUBNET-GROUP', // Optional custom name
       vpcSubnets: {
         subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
       },
     });
 
-    // const ssmRDSSubnetGroup = new ssm.StringParameter(this, 'RDSSubnetGroupSsmParameter', {
-    //   parameterName: '/AWS/CAD/RDS/SUBNET/GROUP',
-    //   stringValue: rdsSubnetGroup.subnetGroupName,
-    // });
-
     new cdk.CfnOutput(this, 'RDSSubnetGroupOutput', {
       value: rdsSubnetGroup.subnetGroupName,
-      exportName: 'Application-RDS-SUBNET-GROUP-Name', // optional if you want to share this output across stacks
+      exportName: 'Application-RDS-SUBNET-GROUP-NAME',
     });
-
   }
 }
